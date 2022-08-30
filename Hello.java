ac1c1c41ebbc71f10ba6d44c9f9167a5388b5729
lierbag1738 <112518341+lierbag1738@users.noreply.github.com>
2022-08-30 17:20:48 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,17 @@
+/*****************************************************************************
+* Hello Java
+******************************************************************************
+* Gabriel McLeod
+* August 25th 2022
+* 255 001
+*****************************************************************************/ 
+public class Hello {
+	public static void main(String[]args) {
+		System.out.println("<Gabriel McLeod>");
+		System.out.println("<Hello Java>");
+		System.out.println("<Program/Project Description>");
+		System.out.println("<August 25th 2022>");
+		System.out.println("<255 001>");
+		System.out.println();
+	}	
+}
